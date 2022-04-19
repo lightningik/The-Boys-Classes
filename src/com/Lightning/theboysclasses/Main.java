b7516a8840ad827a67da1ac32665aede81c5937b
LightningYTlol <73615108+LightningYTlol@users.noreply.github.com>
2022-04-19 15:58:24 -0700

    Started up some of the basic stuff

--- src/com/Lightning/theboysclasses/Main.java
+++ src/com/Lightning/theboysclasses/Main.java
@@ -1,4 +1,20 @@
 package com.Lightning.theboysclasses;
 
-public class Main {
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info(ChatColor.GOLD + "[TBC] Plugin has loaded.");
+    }
+
+
+    @Override
+    public void onDisable(){
+        getLogger().info(ChatColor.BLUE + "[TBC] Plugin shutting down.");
+    }
+
 }
